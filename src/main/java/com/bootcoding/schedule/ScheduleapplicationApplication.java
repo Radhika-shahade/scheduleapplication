f29be4f30c9aeaa9521752599009621bbe9ce663
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-23 15:21:46 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/schedule/ScheduleapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoding.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcoding/schedule/ScheduleapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoding.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
