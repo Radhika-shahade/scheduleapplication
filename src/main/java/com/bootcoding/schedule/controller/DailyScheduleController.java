af037c9d8287f98b00b0a5137f761931f682ab85
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-28 22:20:18 +0530

    schedule app

--- src/main/java/com/bootcoding/schedule/controller/DailyScheduleController.java
+++ src/main/java/com/bootcoding/schedule/controller/DailyScheduleController.java
@@ -1,26 +1,52 @@
 package com.bootcoding.schedule.controller;
 
+//import com.bootcoding.schedule.model.DailySchedule;
+//import com.bootcoding.schedule.service.DailyService;
+
 import com.bootcoding.schedule.model.DailySchedule;
 import com.bootcoding.schedule.service.DailyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/schedule")
 public class DailyScheduleController {
 
     @Autowired
-    private DailyService scheduleService;
+    private DailyService dailyService;
+
+    //to generate random data
+    @PostMapping("/dailySchedule/{week}/{value}")
+    public List<DailySchedule> generateDailySchedule(@PathVariable("week") int weekId, @PathVariable int value ) {
+        return dailyService.saveData(weekId, value);
+    }
 
     @PostMapping("/dailySchedule")
-    public String generateDailySchedule(@RequestBody DailySchedule dailySchedule)
+    public DailySchedule insertData(@RequestBody DailySchedule dailySchedule)
     {
-        scheduleService.saveData(dailySchedule);
-        return "application is running";
+        return dailyService.save(dailySchedule);
     }
+
+
+    //get daily  details according to week id
+    @GetMapping("/week/{weekId}")
+    public List<DailySchedule> getDailySchedulesByWeekId(@PathVariable int weekId) {
+        return dailyService.findByWeekScheduleId(weekId);
+
+    }
+
+    @DeleteMapping("/dailySchedule/{daynumber}")
+
+        public String deleteBYId(@PathVariable int dayId)
+        {
+            dailyService.deleteById(dayId);
+            return "record deleted";
+        }
+
+
 
 
 }
--- src/main/java/com/bootcoding/schedule/service/DailyService.java
+++ src/main/java/com/bootcoding/schedule/service/DailyService.java
@@ -1,9 +1,18 @@
 package com.bootcoding.schedule.service;
 
 import com.bootcoding.schedule.model.DailySchedule;
+import com.bootcoding.schedule.model.WeeklySchedule;
 import com.bootcoding.schedule.repository.DailyRepository;
+import com.bootcoding.schedule.util.CourseTopic;
+import com.bootcoding.schedule.util.DateGenerator;
+import com.bootcoding.schedule.util.ModifiedBy;
+import com.bootcoding.schedule.util.WeekNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DailyService {
@@ -11,65 +20,47 @@ public class DailyService {
     @Autowired
     private DailyRepository scheduleRepository;
 
-    public void saveData(DailySchedule data) {
-        scheduleRepository.save(data);
+    @Autowired
+    private WeeklyService weeklyService;
 
+    public List<DailySchedule> saveData(int weekId, int value) {
+        List<DailySchedule> list = new ArrayList<>();
+        Optional<WeeklySchedule> weeklyData = weeklyService.getWeeklyData(weekId);
+        for (int i = 1; i <= value; i++) {
+            DailySchedule dailySchedule = DailySchedule.builder()
+                    .dayNumber(i)
+                    .title("java basic day " + i)
+                    .description("Day wise topics are given")
+                    .courseTopic(CourseTopic.courseTopic())
+                    .category("learning")
+                    .active("yes")
+                    .createDate(DateGenerator.createDate())
+                    .createBy("bootCoding")
+                    .modifiedDate(DateGenerator.modifiedDate())
+                    .modifiedBy(ModifiedBy.name())
+                    .weeklySchedule(weeklyData.get())
+                    .build();
+            list.add(dailySchedule);
+        }
+        scheduleRepository.saveAll(list);
+        return list;
+    }
+
+    public List<DailySchedule> findByWeekScheduleId(int weekId) {
+        System.out.println(weekId);
+        return scheduleRepository.findByWeeklyScheduleId(weekId);
+    }
+
+    public DailySchedule save(DailySchedule dailySchedule)
+    {
+        return scheduleRepository.save(dailySchedule);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //to delete record
+    public void deleteById(int dayId) {
+        scheduleRepository.deleteById(dayId);
+    }
 
 
 }
