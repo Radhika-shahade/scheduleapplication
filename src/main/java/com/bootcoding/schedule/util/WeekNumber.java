96426fd9576b76c38e36134b28f80bd3501e8de0
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-28 22:22:16 +0530

    schedule app

--- src/main/java/com/bootcoding/schedule/util/WeekNumber.java
+++ src/main/java/com/bootcoding/schedule/util/WeekNumber.java
@@ -11,5 +11,11 @@ public class WeekNumber {
         int max=10;
         return random.nextInt(max-min)+min;
     }
+    public static int calculateWeekNumber(int dayNumber) {
+        // For every 5 days, increment the week number
+        // For example, days 1-5 are in week 1, days 6-10 are in week 2, and so on
+        int weekNumber = (dayNumber - 1) / 5 + 1;
+        return weekNumber;
+    }
 
 }
